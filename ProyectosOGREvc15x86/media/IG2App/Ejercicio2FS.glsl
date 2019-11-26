e08f2d4dc8f8357f48ea5b7a50107c31db1bc3f4
Posna <jomart17@ucm.es>
2019-11-26 13:12:22 +0100

    Las luces no van en la bomb

--- ProyectosOGREvc15x86/media/IG2App/Ejercicio2FS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/Ejercicio2FS.glsl
@@ -4,21 +4,51 @@ uniform sampler2D textura0; // tipo sampler2D para texturas 2D
 uniform sampler2D textura1;
 uniform vec4 InColor;
 uniform vec4 OutColor;
-uniform float FB; // blending factor
-uniform float intLuzAmb; // luz ambiente
 in vec2 vUv0; // out del vertex shader
 out vec4 fFragColor; // out del fragment shader
+in vec3 vWvertex;
+in vec3 vWnormal;
+
+uniform vec3 viewPos;
+
+uniform vec3 lightPosition; // en world space
+uniform vec3 lightAmbient;
+uniform vec3 lightDiffuse;
+uniform vec3 lightSpecular;
+uniform vec3 materialDiffuse; // datos del material
+uniform vec3 materialSpecular;
+uniform float materialShininess;
+
 
 void main() {
     vec3 color0 = vec3(texture(textura0, vUv0)); // acceso a texel
     vec3 color1 = vec3(texture(textura1, vUv0)); // configuración
-    vec3 color = mix(color0, color1, FB);// * intLuzAmb;
-    if(gl_FrontFacing){
-        fFragColor = mix(OutColor, vec4(color, 1.0), FB);
-    }else{
-        fFragColor = mix(InColor, vec4(color, 1.0), FB);
-    }
+   
     if(color1.x > 0.6){
         discard;
+    }
+    // ambient
+    vec3 ambient = lightAmbient * materialDiffuse;
+    // diffuse
+    vec3 norm = normalize(vWnormal);
+
+    if(!gl_FrontFacing)
+        norm = -norm;
+
+    vec3 lightDir = lightPosition;
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightDiffuse * materialDiffuse;
+    // specular
+    vec3 viewDir = normalize(viewPos - vWvertex);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), materialShininess);
+    vec3 specular= spec * lightSpecular * materialSpecular;
+
+    if(gl_FrontFacing){
+        vec3 result= (diffuse) * (vec3(OutColor) * color0);
+        fFragColor = vec4(result, 1.0);
+    } else {
+        vec3 result= (diffuse) * vec3(InColor);
+        fFragColor = vec4(result, 1.0);
     }
 }
--- ProyectosOGREvc15x86/media/IG2App/Ejercicio2VS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/Ejercicio2VS.glsl
@@ -1,11 +1,27 @@
 #version 330 core
 
 in vec4 vertex; // atributos de los vértices a procesar
+in vec3 normal;
 in vec2 uv0;
-uniform mat4 modelViewProjMat; // constante de programa
+
+out vec3 vWvertex; // en world space
+out vec3 vWnormal;
+
 out vec2 vUv0; // out del vertex shader
+
+uniform mat4 model; // constante de programa
+uniform mat4 view;
+uniform mat4 proj;
+uniform mat4 normalMat;
+//uniform mat4 projection;
+
 
 void main() {
     vUv0 = uv0;
-    gl_Position = modelViewProjMat * vertex; // obligatorio
+    
+    vec3 vWvertex = vec3(model * vertex); // en view space
+    vec3 vWnormal = mat3(normalMat) * normal;
+
+    gl_Position = proj * view * vec4(vWvertex, 1.0);
+    //gl_Position = modelViewProjMat * vertex; // obligatorio
 }
